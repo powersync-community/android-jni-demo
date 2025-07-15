adce705e5e7f1c85948145a0561645b7f7b87802
Simon Binder <simon@journeyapps.com>
2025-07-15 16:57:54 +0200

    Show text on start

--- app/src/main/java/com/powersync/demo/android/jni/MainActivity.kt
+++ app/src/main/java/com/powersync/demo/android/jni/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -31,17 +38,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
-    PowerSyncJNIDemoApplicationTheme {
+    var didStartDemo by remember { mutableStateOf(false) }
+    val context = LocalContext.current
 
+    fun startDemo() {
+        didStartDemo = true
+        NativeBridge.start(context)
+    }
+
+    PowerSyncJNIDemoApplicationTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Box(
                 modifier = Modifier.fillMaxSize().padding(innerPadding),
                 contentAlignment = Alignment.Center,
             ) {
-                val context = LocalContext.current
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Button(
+                        onClick = ::startDemo,
+                        enabled = !didStartDemo,
+                    ) {
+                        Text("Click to start native demo")
+                    }
 
-                Button(onClick = { NativeBridge.start(context) }) {
-                    Text("Click to start native demo")
+                    if (didStartDemo) {
+                        Text("Demo started, see logcat for outputs")
+                    }
                 }
             }
         }
